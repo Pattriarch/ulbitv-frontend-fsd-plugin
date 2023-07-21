2609487b56d808a746454f32d4c68d29695bb67c
daniilpereverzev <pgelo298@mail.ru>
2023-07-21 15:41:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# eslint-plugin-ulbitv-fsd
+
+Plugin for production project
+
+## Installation
+
+You'll first need to install [ESLint](https://eslint.org/):
+
+```sh
+npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-ulbitv-fsd`:
+
+```sh
+npm install eslint-plugin-ulbitv-fsd --save-dev
+```
+
+## Usage
+
+Add `ulbitv-fsd` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "ulbitv-fsd"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "ulbitv-fsd/rule-name": 2
+    }
+}
+```
+
+## Rules
+
+<!-- begin auto-generated rules list -->
+TODO: Run eslint-doc-generator to generate the rules list.
+<!-- end auto-generated rules list -->
+
+
---
+++ tests/lib/rules/path-validator.js
@@ -0,0 +1,48 @@
+/**
+ * @fileoverview Validates that path works as described in FSD
+ * @author Daniil
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/path-validator"),
+  RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parserOptions: {ecmaVersion: 6, sourceType: 'module'}
+});
+ruleTester.run("path-validator", rule, {
+  valid: [
+    {
+      filename: 'C:\\Users\\pattriarch\\Desktop\\ulbitv-frontend\\src\\entities\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slices/addCommentFormSlice'",
+      errors: [{ message: "В рамках одного слайса все пути должны быть относительны" }],
+    },
+  ],
+
+  invalid: [
+    {
+      filename: 'C:\\Users\\pattriarch\\Desktop\\ulbitv-frontend\\src\\entities\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from 'entities/Article/model/slices/addCommentFormSlice'",
+      errors: [{ message: "В рамках одного слайса все пути должны быть относительны" }],
+    },
+    {
+      filename: 'C:\\Users\\pattriarch\\Desktop\\ulbitv-frontend\\src\\entities\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/slices/addCommentFormSlice'",
+      errors: [{ message: "В рамках одного слайса все пути должны быть относительны" }],
+      options: [
+        {
+          alias: '@'
+        }
+      ]
+    },
+  ],
+});
